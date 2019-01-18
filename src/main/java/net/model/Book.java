b39ab3bdb2037f6651bf11f6628a2c1dc15d3edc
MASurganova <masurganova@gmail.com>
2019-01-18 12:25:31 +0300

    model

---
+++ src/main/java/net/model/Book.java
@@ -0,0 +1,99 @@
+package net.proselyte.bookmanager.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BOOKS")
+public class Book {
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "BOOK_TITLE")
+    private String bookTitle;
+
+    @Column(name = "BOOK_DESCRIPTION")
+    private String bookDescription;
+
+    @Column(name = "BOOK_AUTHOR")
+    private String bookAuthor;
+
+    @Column(name = "BOOK_ISBN")
+    private String bookISBN;
+
+    @Column(name = "BOOK_PRINT_YEAR")
+    private int printYear;
+
+    @Column(name = "BOOK_STATUS")
+    private boolean readAlready;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookDescription() {
+        return bookDescription;
+    }
+
+    public void setBookDescription(String bookDescription) {
+        this.bookDescription = bookDescription;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookISBN() {
+        return bookISBN;
+    }
+
+    public void setBookISBN(String bookISBN) {
+        this.bookISBN = bookISBN;
+    }
+
+    public int getPrintYear() {
+        return printYear;
+    }
+
+    public void setPrintYear(int printYear) {
+        this.printYear = printYear;
+    }
+
+    public boolean isReadAlready() {
+        return readAlready;
+    }
+
+    public void setReadAlready(boolean readAlready) {
+        this.readAlready = readAlready;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", bookDescription='" + bookDescription + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookISBN=" + bookISBN +
+                ", printYear=" + printYear +
+                ", readAlready=" + readAlready +
+                '}';
+    }
+}
